da7536cb91bdf63a0d5c367b3efbff02eb60c745
Mike Donovan <mike@mikedonovan.dev>
2023-02-10 02:47:32 -0800

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import '../css/style.css';
+
+window.addEventListener('load', () => {
+  document.body.innerHTML = '<p>Guacamole</p>';
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000,
+    aggregateTimeout: 600,
+  },
+  entry: {
+    index: './src/js/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    client: {
+      overlay: false,
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    minimize: true,
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+      new HtmlMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new ESLintPlugin({
+      fix: true,
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
