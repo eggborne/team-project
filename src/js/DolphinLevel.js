3d2cdfe7b23e0f3ec9d7c139b71730a55c6b04a2
Mike Donovan <mike@mikedonovan.dev>
2023-02-14 11:01:24 -0800

    Add DolphinLevel

---
+++ src/js/DolphinLevel.js
@@ -0,0 +1,38 @@
+import '../css/DolphinLevel.css';
+import { randomInt } from './util.js';
+
+export default class DolphinLevel {
+  constructor(className) {
+    document.body.classList = [className];
+    this.wordsPerLengthInWave = 6;
+    this.wordLengths = [5,6,7];
+    this.shipSpeed = 5000;
+    this.launchFrequency = 3000;
+  }
+
+  firstFocusAction(ship) {
+    //
+  }
+
+  maintainFocusAction(ship) {
+    //
+  }
+
+  async destroyShipAction(ship) {
+    this.game.destroyShip(ship, true);
+  }
+
+  loseFocusAction(ship) {
+    
+  }
+
+  placeWordShip() {
+    let shipPositionX = '-18rem';
+    let shipPositionY = randomInt(window.innerHeight * 0.2, window.innerHeight * 0.8) + 'px';
+    return {
+      x: shipPositionX,
+      y: shipPositionY,
+    };
+  }
+
+}
