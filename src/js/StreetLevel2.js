80bc1f316ee43f20cff8ce4d63374df819b98769
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-02-16 12:24:36 -0800

    add blanka gif

---
+++ src/js/StreetLevel2.js
@@ -0,0 +1,70 @@
+import '../css/StreetLevel2.css';
+
+export default class Level2 {
+  constructor(className) {
+    document.body.classList = [className];
+    this.wordsPerLengthInWave = 5;
+    this.wordLengths = [5, 6];
+    this.shipSpeed = 2800;
+    this.launchFrequency = 3000;
+    this.flameEmoji1 = document.createElement('div');
+    this.flameEmoji1.id = 'flameEmoji01';
+    this.flameEmoji1.innerHTML = '&#128293';
+
+    this.flameEmoji2 = document.createElement('div');
+    this.flameEmoji2.id = 'flameEmoji02';
+    this.flameEmoji2.innerHTML = '&#128293';
+
+    this.flameEmoji3 = document.createElement('div');
+    this.flameEmoji3.id = 'flameEmoji03';
+    this.flameEmoji3.innerHTML = '&#128293';
+
+    this.flameEmoji4 = document.createElement('div');
+    this.flameEmoji4.id = 'flameEmoji04';
+    this.flameEmoji4.innerHTML = '&#128293';
+
+    this.figher = document.createElement('div');
+    this.figher.id = 'fighter01';
+
+
+    document.querySelector('main').append(this.flameEmoji1, this.flameEmoji2,
+      this.flameEmoji3, this.flameEmoji4, this.figher);
+
+  }
+
+  firstFocusAction(ship) {
+    //
+  }
+
+  maintainFocusAction(ship) {
+    //
+  }
+
+  async destroyShipAction(ship) {
+    this.flameEmoji1.classList.add('showing');
+    this.flameEmoji2.classList.add('showing');
+    this.flameEmoji3.classList.add('showing');
+    this.flameEmoji4.classList.add('showing');
+    setTimeout(() => {
+      this.flameEmoji1.classList.remove('showing');
+      this.flameEmoji2.classList.remove('showing');
+      this.flameEmoji3.classList.remove('showing');
+      this.flameEmoji4.classList.remove('showing');
+    }, 500);
+    this.game.destroyShip(ship, true);
+  }
+
+  loseFocusAction(ship) {
+    //
+  }
+
+  placeWordShip() {
+    let shipPositionX = '50%';
+    let shipPositionY = '50%';
+    return {
+      x: shipPositionX,
+      y: shipPositionY,
+    };
+  }
+
+}
